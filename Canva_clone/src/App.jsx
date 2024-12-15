39b6cebb6310524a2d53a46b117b63ae5d345658
singhpraveen77 <singhpraveen8893@gmail.com>
2024-12-15 23:45:04 +0530

    navbar ready

---
+++ Canva_clone/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+
+function App() {
+
+  return (
+  
+      <div className="w-full h-screen flex-column align-center justify-center  ">
+        <Navbar />
+        <Hero />
+
+
+        </div>
+    
+  )
+}
+
+export default App
---
+++ Canva_clone/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="w-[80%] h-[80%] bg-sky-300 rounded-lg flex items-center justify-center translate-x-[10%]">
+      {/* Content goes here */}
+    </div>
+  )
+}
+
+export default Hero
